dab82a83f0986a9543288aff054d11691d9cfda5
wluc9875 <lwalterthum@mac.com>
2021-04-11 15:12:15 +0200

    configuration of actions triggered by each button

--- iUFC/AJS37ViewController.swift
+++ iUFC/AJS37ViewController.swift
@@ -21,6 +21,32 @@ import UIKit
 class AJS37ViewController: PlaneViewController {
     override func initActions() {
         actions = [
+            Action(type: .pushButton, deviceId: 12, commandId: 3020), // NUM0 (0)
+            Action(type: .pushButton, deviceId: 12, commandId: 3021), // NUM1 (1)
+            Action(type: .pushButton, deviceId: 12, commandId: 3022), // NUM2 (2)
+            Action(type: .pushButton, deviceId: 12, commandId: 3023), // NUM3 (3)
+            Action(type: .pushButton, deviceId: 12, commandId: 3024), // NUM4 (4)
+            Action(type: .pushButton, deviceId: 12, commandId: 3025), // NUM5 (5)
+            Action(type: .pushButton, deviceId: 12, commandId: 3026), // NUM6 (6)
+            Action(type: .pushButton, deviceId: 12, commandId: 3027), // NUM7 (7)
+            Action(type: .pushButton, deviceId: 12, commandId: 3028), // NUM8 (8)
+            Action(type: .pushButton, deviceId: 12, commandId: 3029), // NUM9 (9)
+            Action(type: .pushButton, deviceId: 23, commandId: 3008, argument: 0.0), // INUT => UT(10)
+            Action(type: .pushButton, deviceId: 12, commandId: 3011), // WPT B1 (11)
+            Action(type: .pushButton, deviceId: 12, commandId: 3012), // WPT B1 (12)
+            Action(type: .pushButton, deviceId: 12, commandId: 3013), // WPT B1 (13)
+            Action(type: .pushButton, deviceId: 12, commandId: 3014), // WPT B1 (14)
+            Action(type: .pushButton, deviceId: 12, commandId: 3015), // WPT B1 (15)
+            Action(type: .pushButton, deviceId: 12, commandId: 3016), // WPT B1 (16)
+            Action(type: .pushButton, deviceId: 12, commandId: 3017), // WPT B1 (17)
+            Action(type: .pushButton, deviceId: 12, commandId: 3018), // WPT B1 (18)
+            Action(type: .pushButton, deviceId: 12, commandId: 3019), // WPT B1 (19)
+            Action(type: .pushButton, deviceId: 12, commandId: 3010), // WPT BX (20)
+            Action(type: .pushButton, deviceId: 12, commandId: 3009), // WPT LSSKU (21)
+            Action(type: .pushButton, deviceId: 12, commandId: 3008), // WPT LMAL (22)
+            Action(type: .pushButton, deviceId: 23, commandId: 3011, argument: -1.0), // data selector CCW (23)
+            Action(type: .pushButton, deviceId: 23, commandId: 3011, argument: 1.0), // data selector CW (24)
+            Action(type: .pushButton, deviceId: 23, commandId: 3008, argument: 1.0), // INUT => IN (25)
         ]
     }
     
