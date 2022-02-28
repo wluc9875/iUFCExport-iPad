ee21d65e9f66d13d1b3b009eabf0bcd5f61cce49
wluc9875 <lwalterthum@mac.com>
2022-02-28 18:31:43 +0100

    - configured F16 CMS alternate images - synchronized DCS and iPad switches

--- iUFC/F16CMSViewControlller.swift
+++ iUFC/F16CMSViewControlller.swift
@@ -19,6 +19,34 @@
 import UIKit
 
 class F16CMSViewControlller: PlaneViewController {
+    @IBOutlet var prgmRotatorImageView: UIImageView!
+    @IBOutlet var modeRotatorImageView: UIImageView!
+    @IBOutlet var rwrButton: UIButton!
+    @IBOutlet var jmrButton: UIButton!
+    @IBOutlet var mwsButton: UIButton!
+    @IBOutlet var jettButton: UIButton!
+    @IBOutlet var c01Button: UIButton!
+    @IBOutlet var c02Button: UIButton!
+    @IBOutlet var chButton: UIButton!
+    @IBOutlet var flButton: UIButton!
+    
+    static let prgmRotatorImageNames = [
+        //0.0: nil, // default image
+        1.0: "f16/F16-CMS-prgm1",
+        2.0: "f16/F16-CMS-prgm2",
+        3.0: "f16/F16-CMS-prgm3",
+        4.0: "f16/F16-CMS-prgm4",
+    ]
+    
+    static let modeRotatorImageNames = [
+        //0.0: nil, // default image
+        1.0: "f16/F16-CMS-mode1",
+        2.0: "f16/F16-CMS-mode2",
+        3.0: "f16/F16-CMS-mode3",
+        4.0: "f16/F16-CMS-mode4",
+        5.0: "f16/F16-CMS-mode5"
+    ]
+    
     override func initActions() {
         actions = [
             Action(type: .toggleButton, deviceId: 32, commandId: 3001), // RWR switch (0)
@@ -34,5 +62,43 @@ class F16CMSViewControlller: PlaneViewController {
             Action(type: .pushButton, deviceId: 32, commandId: 3021, argument: -0.1), // MODE- switch (10)
             Action(type: .pushButton, deviceId: 32, commandId: 3021, argument: 0.1), // MODE+ switch (11)
         ]
+    }
+    
+    override func updateDisplays(with content: [String: String]) {
+        let cmsSwitchStates = (content["cmsswitches"] ?? "").split(separator: " ")
+        let rwrOn = (cmsSwitchStates[0] == "1")
+        actions[0].argument = rwrOn ? 1.0 : 0.0
+        let jmrOn = (cmsSwitchStates[1] == "1")
+        actions[1].argument = jmrOn ? 1.0 : 0.0
+        let mwsOn = (cmsSwitchStates[2] == "1")
+        actions[2].argument = mwsOn ? 1.0 : 0.0
+        let jettOn = (cmsSwitchStates[3] == "1")
+        actions[3].argument = jettOn ? 1.0 : 0.0
+        let c01On = (cmsSwitchStates[4] == "1")
+        actions[4].argument = c01On ? 1.0 : 0.0
+        let c02On = (cmsSwitchStates[5] == "1")
+        actions[5].argument = c02On ? 1.0 : 0.0
+        let chOn = (cmsSwitchStates[6] == "1")
+        actions[6].argument = chOn ? 1.0 : 0.0
+        let flOn = (cmsSwitchStates[7] == "1")
+        actions[7].argument = flOn ? 1.0 : 0.0
+        let prgm = (Double(cmsSwitchStates[8])! * 10.0).rounded()
+        let mode = (Double(cmsSwitchStates[9])! * 10.0).rounded()
+        DispatchQueue.main.async {
+            self.rwrButton.isSelected = rwrOn
+            self.jmrButton.isSelected = jmrOn
+            self.mwsButton.isSelected = mwsOn
+            self.jettButton.isSelected = jettOn
+            self.c01Button.isSelected = c01On
+            self.c02Button.isSelected = c02On
+            self.chButton.isSelected = chOn
+            self.flButton.isSelected = flOn
+            if let prgmRotatorImageName = F16CMSViewControlller.prgmRotatorImageNames[prgm] {
+                self.prgmRotatorImageView.image = UIImage(named: prgmRotatorImageName)
+            }
+            if let modeRotatorImageName = F16CMSViewControlller.modeRotatorImageNames[mode] {
+                self.modeRotatorImageView.image = UIImage(named: modeRotatorImageName)
+            }
+        }
     }
 }
