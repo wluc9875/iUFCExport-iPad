3d934e208a2310e9a062f352e217201b2c68d084
wluc9875 <lwalterthum@mac.com>
2022-03-01 17:18:55 +0100

    completed AJS37 CMS

--- iUFC/AJS37CMSViewController.swift
+++ iUFC/AJS37CMSViewController.swift
@@ -26,6 +26,42 @@ class AJS37CMSViewController: PanelViewController {
     @IBOutlet var rwrImageView: UIImageView!
     @IBOutlet var chflImageView: UIImageView!
     
+    static let cmsModeImagesNames = [
+        //0.0: nil, // default image
+        1.0: "AJS37/AJS37-CMS-rotator1_0",
+        2.0: "AJS37/AJS37-CMS-rotator1_1",
+        3.0: "AJS37/AJS37-CMS-rotator1_2",
+        4.0: "AJS37/AJS37-CMS-rotator1_3",
+    ]
+    
+    static let chflImagesNames = [
+        //0.0: nil, // default image
+        1.0: "AJS37/AJS37-CMS-rotator2_RF",
+        2.0: "AJS37/AJS37-CMS-rotator2_F",
+    ]
+    
+    static let rwrModeImagesNames = [
+        //0.0: nil, // default image
+        1.0: "AJS37/AJS37-CMS-rotator3_1",
+        2.0: "AJS37/AJS37-CMS-rotator3_2",
+    ]
+    
+    static let jammerModeImagesNames = [
+        //0.0: nil, // default image
+        1.0: "AJS37/AJS37-CMS-rotator5_1",
+        2.0: "AJS37/AJS37-CMS-rotator5_2",
+        3.0: "AJS37/AJS37-CMS-rotator5_3",
+        4.0: "AJS37/AJS37-CMS-rotator5_4",
+    ]
+    
+    static let jammerBandImagesNames = [
+        //0.0: nil, // default image
+        1.0: "AJS37/AJS37-CMS-rotator6_1",
+        2.0: "AJS37/AJS37-CMS-rotator6_2",
+        3.0: "AJS37/AJS37-CMS-rotator6_3",
+        4.0: "AJS37/AJS37-CMS-rotator6_4",
+    ]
+    
     override func initActions() {
         actions = [
             Action(type: .rotatorCCW, deviceId: 28, commandId: 3024, argument: 0.0, increment: 0.1, minimum: 0.0, maximum: 0.4), // Jammer Mode - switch (0)
@@ -41,5 +77,59 @@ class AJS37CMSViewController: PanelViewController {
             Action(type: .pushButton, deviceId: 28, commandId: 3027, argument: 0.0), // CMS Streak - switch (10)
             Action(type: .pushButton, deviceId: 28, commandId: 3027, argument: 0.1), // CMS Streak + switch (11)
         ]
+    }
+    
+    override func updateDisplays(with content: [String: String]) {
+        let cmsSwitchStates = (content["cmsswitches"] ?? "").split(separator: " ")
+        let jammerMode = (Double(cmsSwitchStates[0])! * 10.0).rounded()
+        let jammerBand = (Double(cmsSwitchStates[1])! * 10.0).rounded()
+        let cmsMode = (Double(cmsSwitchStates[2])! * 10.0).rounded()
+        let rwrMode = (Double(cmsSwitchStates[3])! * 10.0).rounded()
+        let chfl = (Double(cmsSwitchStates[4])! * 10.0).rounded()
+        let streak = (Double(cmsSwitchStates[5])! * 10.0).rounded()
+                
+        actions[0].argument = jammerMode / 10.0
+        actions[1].argument = jammerMode / 10.0
+        actions[2].argument = jammerBand / 10.0
+        actions[3].argument = jammerBand / 10.0
+        actions[4].argument = cmsMode / 10.0
+        actions[5].argument = cmsMode / 10.0
+        actions[6].argument = rwrMode / 10.0
+        actions[7].argument = rwrMode / 10.0
+        actions[8].argument = chfl / 10.0
+        actions[9].argument = chfl / 10.0
+        
+        DispatchQueue.main.async {
+            if let jammerImageName = AJS37CMSViewController.jammerModeImagesNames[jammerMode] {
+                self.jammerModeImageView.image = UIImage(named: jammerImageName)
+            } else {
+                self.jammerModeImageView.image = nil
+            }
+            if let jammerBandImageName = AJS37CMSViewController.jammerBandImagesNames[jammerBand] {
+                self.jammerBandImageView.image = UIImage(named: jammerBandImageName)
+            } else {
+                self.jammerBandImageView.image = nil
+            }
+            if let cmsModeImageName = AJS37CMSViewController.cmsModeImagesNames[cmsMode] {
+                self.cmsModeImageView.image = UIImage(named: cmsModeImageName)
+            } else {
+                self.cmsModeImageView.image = nil
+            }
+            if let rwrModeImageName = AJS37CMSViewController.rwrModeImagesNames[rwrMode] {
+                self.rwrImageView.image = UIImage(named: rwrModeImageName)
+            } else {
+                self.rwrImageView.image = nil
+            }
+            if let chflImageName = AJS37CMSViewController.chflImagesNames[chfl] {
+                self.chflImageView.image = UIImage(named: chflImageName)
+            } else {
+                self.chflImageView.image = nil
+            }
+            if streak > 0 {
+                self.streakImageView.image = UIImage(named: "AJS37/AJS37-CMS-rotator4")
+            } else {
+                self.streakImageView.image = nil
+            }
+        }
     }
 }
